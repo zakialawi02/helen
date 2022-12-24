ff1baa9e603a6705931a88109867dc35d4f8eaaa
slowy07 <slowy.arfy@gmail.com>
2022-12-25 01:28:53 +0700

    fix: update tum utils
    error tumutils error message:  object is not callable
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- helen/datasets/tumutils.py
+++ helen/datasets/tumutils.py
@@ -1,48 +1,3 @@
-#!/usr/bin/python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, Juergen Sturm, TUM
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of TUM nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-"""
-Slightly modified version of the "associate.py" and "evaluate_rpe.py" helper provided by TUM at: 
-https://svncvpr.in.tum.de/cvpr-ros-pkg/trunk/rgbd_benchmark/rgbd_benchmark_tools/src/rgbd_benchmark_tools/associate.py
-https://svncvpr.in.tum.de/cvpr-ros-pkg/trunk/rgbd_benchmark/rgbd_benchmark_tools/src/rgbd_benchmark_tools/evaluate_rpe.py
-
-The Kinect provides the color and depth images in an un-synchronized way. This means that the set of time stamps from
-the color images do not intersect with those of the depth images. Therefore, we need some way of associating color
-images to depth images.
-
-This script contains helpers for reading the time stamps from e.g. the "rgb.txt" file and the "depth.txt" file, 
-and joining them by finding the best matches.
-"""
 from typing import Optional
 import sys
 
@@ -74,9 +29,10 @@ def transform44(l: tuple):
     if nq < _EPS:
         return np.array(
             (
-                (1.0, 0.0, 0.0, t[0])(0.0, 1.0, 0.0, t[1])(0.0, 0.0, 1.0, t[2])(
-                    0.0, 0.0, 0.0, 1.0
-                )
+                (1.0, 0.0, 0.0, t[0]),
+                (0.0, 1.0, 0.0, t[1]),
+                (0.0, 0.0, 1.0, t[2]),
+                (0.0, 0.0, 0.0, 1.0),
             ),
             dtype=np.float64,
         )
